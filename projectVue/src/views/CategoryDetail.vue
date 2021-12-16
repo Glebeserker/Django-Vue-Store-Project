78c986ff10281fdd9d8089129e49980da2ba7157
Gleb Belyaev <71087238+Glebeserker@users.noreply.github.com>
2021-12-16 09:13:25 -0600

    Update CategoryDetail.vue

--- projectVue/src/views/CategoryDetail.vue
+++ projectVue/src/views/CategoryDetail.vue
@@ -1,25 +1,17 @@
 <template>
     <div class='category-detail'>
-        <section class='hero is-medium is-dark mb-6'>
+        
+        <section class='hero is-small is-dark mb-6'>
             <div class='hero-body has-text-centered'>
                 <p class='title mb-6'>
-                    {{  }}
+                    {{ categoryDetail.name }}
                 </p>
             </div>
         </section>
-
-        <div class='column is-multiline'>
-            <div class='column is-9' v-for='product in categoryDetail' v-bind:key='product.id'>
-                <div class='box'>
-                    <figure class='image mb-6'>
-                        <img v-bind:src="product.get_thumbnail">
-                    </figure>
-
-                    <h1 class='title'>{{ categoryDetail.product.name }}</h1>
-                    <p><strong>Price: </strong>$ {{ categoryDetail.product.price }}</p>
-                    <router-link v-bind:to='categoryDetail.product.get_absolute_url' class='button is-dark mt-4'>View Product</router-link>
-                </div>
-            </div>
+        <div class='columns is-multiline '>
+        <ProductsArea v-for="product in categoryDetail.products"
+        v-bind:key='product.id'
+        v-bind:product='product' />
         </div>
     </div>
 </template>
@@ -27,18 +19,19 @@
 <script>
 import axios from 'axios'
 
+import ProductsArea from '@/components/ProductsArea'
+
 export default {
     name: 'CategoryDetail',
+    components: {
+        ProductsArea
+    },
     data(){
         return {
             categoryDetail: {
-                product: []
-            },
-            
+                products: []
+            }
         }
-    },
-    props: {
-        categoryDetail: Object
     },
     mounted(){
         this.getCategoryDetail()
