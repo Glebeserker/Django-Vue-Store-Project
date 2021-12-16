31b999a32482172a52c60778306abd16f073e2ad
Gleb Belyaev <71087238+Glebeserker@users.noreply.github.com>
2021-12-16 11:00:37 -0600

    Update ProductDetail.vue

--- projectVue/src/views/ProductDetail.vue
+++ projectVue/src/views/ProductDetail.vue
@@ -2,17 +2,17 @@
     <div class='product-detail'>
         <div class="box">
             <div class="columns is-multiline">
-                <div class="column is-three-quarters">
+                <div class="column is-full-mobile is-full-touch is-three-quarters-desktop">
                     <figure class='image'>
                         <img v-bind:src="product.get_image">
                     </figure>
                 </div>
-                <div class="column is-one-fourth has-text-centered">
-                    <h1 class="title mb-6">{{ product.name }}</h1>
+                <div class="column is-one-fourth is-full-mobile is-full-touch has-text-centered">
+                    <h1 class="title mb-4 is-size-1">{{ product.name }}</h1>
                     <h3 class="is-size-3 mb-4">Price:  ${{ product.price }}</h3>
                     <h4 class='is-size-3'>Description: </h4>
-                    <p class='is-size-4'>{{product.description}}</p>
-                    <button class="button mt-6 is-medium is-dark">Add To Cart</button>
+                    <p class=''>{{product.description}}</p>
+                    <button class="button mt-4 is-medium is-dark">Add To Cart</button>
                 </div>
             </div>
         </div>
